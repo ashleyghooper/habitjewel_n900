1fe5b1bbad1f625c53930d2469361357285ed2d9
Ashley Hooper <ashleyghooper@gmail.com>
2016-08-10 20:43:49 +1200

    Fixed exclusion filter on auto_deploy.sh

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -19,7 +19,7 @@ cd $local_project_path
 echo "Waiting for file updates in $local_project_path to deploy to $remote_host"
 
 inotifywait -r -mq --timefmt '%d/%m/%y %H:%M' --format '%T %w %f %e' \
-    -e modify . --exclude '(.*.sw*|.git)' | while read date time dir file event
+    -e modify . --exclude '(.*\.sw.|\.git)' | while read date time dir file event
 do
     event=${event}
     affected_file=${dir}${file}
